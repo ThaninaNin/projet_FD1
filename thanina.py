1c14b303b69bb4e7a87588b9f8f8c701bfde9bdb
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2023-04-15 16:10:34 +0100

    Add files via upload

--- thanina.py
+++ thanina.py
@@ -1,25 +1,66 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+# elbow pour K_means
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+def elbow(file_path):
+
+  data = pd.read_csv(file_path, delimiter=';')
+  # PREPROCESSING  AND NORMALISATION DES DONNées
+  le = LabelEncoder()
+  for col in data.columns:
+    if data[col].dtype == 'object':
+      data[col] = le.fit_transform(data[col].astype(str))
+  imp = SimpleImputer(strategy='mean')
+  data = pd.DataFrame(imp.fit_trans)
+
+  scaler = MinMaxScaler()
+  data1 = pd.DataFrame(scaler.fit_transform(data.select_dtypes(include=np.number)), columns=data.select_dtypes(include=np.number).columns)
+
+
+
+  
+
+  # Calcul de la variance expliquée pour différents nombres de classes
+  variances = []
+  for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(data1)
+    variances.append(kmeans.inertia_)
+
+  # Tracé de la courbe du coude
+  plt.plot(range(1, 11), variances)
+  plt.xlabel('Nombre de classes')
+  plt.ylabel('Variance expliquée')
+  plt.title('Courbe du coude pour K-means')
+  plt.show()
+
+  st.write(plt)
+
+
+  # Détermination du nombre optimal de classes
+  diff_variances = np.diff(variances)
+  diff2_variances = np.diff(diff_variances)
+  n_clusters = np.argmin(diff2_variances) + 2
+  st.write(f"Le nombre optimal de classes est {n_clusters}")
+
+
+
+
 
 
 st.sidebar.title("Paramétres")
-k = st.sidebar.slider('precise K', 0, 200, 1)
+
+
+st.title("Projet Fouille de Données")
+uploaded_file=st.file_uploader("Veuillez chosir votre dataset")
+if uploaded_file is not None:
+  elbow(uploaded_file)

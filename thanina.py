92f3cbe0b1b9b79d0e8640eb832af189454e3fb9
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2023-04-15 18:33:58 +0100

    Add files via upload

--- thanina.py
+++ thanina.py
@@ -7,60 +7,113 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import pairwise_distances
+import time
 
 
-# elbow pour K_means
 
 
-def elbow(file_path):
+st.set_page_config(
+    page_title="Projet Fouille de Données",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
+def preprocessing(file_path):
   data = pd.read_csv(file_path, delimiter=';')
-  # PREPROCESSING  AND NORMALISATION DES DONNées
+  # Preprocess data
   le = LabelEncoder()
   for col in data.columns:
-    if data[col].dtype == 'object':
-      data[col] = le.fit_transform(data[col].astype(str))
+      if data[col].dtype == 'object':
+          data[col] = le.fit_transform(data[col].astype(str))
   imp = SimpleImputer(strategy='mean')
-  data = pd.DataFrame(imp.fit_trans)
-
+  data = pd.DataFrame(imp.fit_transform(data), columns=data.columns)
   scaler = MinMaxScaler()
   data1 = pd.DataFrame(scaler.fit_transform(data.select_dtypes(include=np.number)), columns=data.select_dtypes(include=np.number).columns)
+  return data,data1
 
 
+
+def kMean(k,data,data1):
+  # Cluster data
+  kmeans = KMeans(n_clusters=k, n_init=10)
+  kmeans.fit(data1)
+
+  # Dictionnaire pour stocker les indices de chaque cluster
+  clusters = {}
+  for i, label in enumerate(kmeans.labels_):
+    if label not in clusters:
+      clusters[label] = [i]
+    else:
+      clusters[label].append(i)
 
   
 
-  # Calcul de la variance expliquée pour différents nombres de classes
-  variances = []
-  for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(data1)
-    variances.append(kmeans.inertia_)
+  # Calcul de l'intraclasse
+  intraclasse = kmeans.inertia_
 
-  # Tracé de la courbe du coude
-  plt.plot(range(1, 11), variances)
-  plt.xlabel('Nombre de classes')
-  plt.ylabel('Variance expliquée')
-  plt.title('Courbe du coude pour K-means')
-  plt.show()
+  # Calcul de l'interclasse
+  interclasse = pairwise_distances(kmeans.cluster_centers_, Y=None, metric='euclidean').sum()
 
-  st.write(plt)
+  return clusters,intraclasse,interclasse
 
-
-  # Détermination du nombre optimal de classes
-  diff_variances = np.diff(variances)
-  diff2_variances = np.diff(diff_variances)
-  n_clusters = np.argmin(diff2_variances) + 2
-  st.write(f"Le nombre optimal de classes est {n_clusters}")
+  
 
 
 
 
 
-
-st.sidebar.title("Paramétres")
 
 
 st.title("Projet Fouille de Données")
 uploaded_file=st.file_uploader("Veuillez chosir votre dataset")
 if uploaded_file is not None:
-  elbow(uploaded_file)
+  
+  
+  
+  
+
+  
+  data,data1 = preprocessing(uploaded_file)
+
+
+  progress_text = "Attendez le préprocessing"
+  my_bar = st.progress(0, text=progress_text)
+
+  for percent_complete in range(100):
+    time.sleep(0.00001)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+
+  st.success("Préprocessing avec succée", icon="✅")
+
+  st.sidebar.title("Paramétres")
+  methodes = st.sidebar.multiselect('Quelle méthode voulez vous utilisez',["Suprevisée","Non-Supervisée"])
+
+  if  "Non-Supervisée" in methodes :
+    methode = st.sidebar.multiselect('Quelle approche voulez vous utilisez',["K-Means","K-Medoid","Diana","Agnes","Dbscan"])
+    if  "K-Means" in methode :
+
+      k = st.sidebar.slider('Choisir le nombre de cluster', 1, 30, 1)
+      
+      cluster = ["Tous"]
+      for i in range(1,k+1):
+        label = "Cluster : "
+        labels = f"{label}{i}"
+        cluster.append(labels)
+
+
+      clusters = st.sidebar.multiselect('Quel Cluster voullez vous afficher',cluster)
+      items,intraclasse,interclasse = kMean(k,data,data1)
+
+      if "Tous" in clusters :
+        # Affichage des éléments de chaque cluster
+        for label, indices in items.items():
+          st.write("Cluster : ",label,data.iloc[indices])
+        st.success(f"Intraclasse : {intraclasse}", icon="✅")
+        st.success(f"Interclasse : {interclasse}", icon="✅")
+
+
+      
+
+  
